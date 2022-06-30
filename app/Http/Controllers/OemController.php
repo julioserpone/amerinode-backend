0ded515790c288be56a5ec7afd2b25af532b2c2d
Julio Cesar Hernandez <juliohernandezs@gmail.com>
2022-06-30 14:58:13 -0500

    feat: migration, model, request and controller for OEM

---
+++ app/Http/Controllers/OemController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Oem;
+use App\Http\Requests\StoreOemRequest;
+use App\Http\Requests\UpdateOemRequest;
+
+class OemController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreOemRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreOemRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Oem  $oem
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Oem $oem)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Oem  $oem
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Oem $oem)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateOemRequest  $request
+     * @param  \App\Models\Oem  $oem
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateOemRequest $request, Oem $oem)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Oem  $oem
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Oem $oem)
+    {
+        //
+    }
+}
---
+++ database/migrations/2022_06_30_173936_create_oems_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('oems', function (Blueprint $table) {
+            $table->id();
+            $table->integer('OemId');
+            $table->string('description');
+            $table->string('status')->default('active');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('oems');
+    }
+};
