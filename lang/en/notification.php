a1bd726e25f91a258b6271603e5748322ee51d87
Julio Cesar Hernandez <juliohernandezs@gmail.com>
2022-07-04 16:24:48 -0500

    fix: deleted notification message changed to inactivated

--- lang/en/notification.php
+++ lang/en/notification.php
@@ -4,5 +4,6 @@ return [
     'updated' => 'Successfully updated :attribute',
     'created' => 'Successfully created :attribute',
     'deleted' => 'Successfully deleted :attribute',
+    'inactivated' => 'Successfully inactivated :attribute',
     'duplicated' => 'It is not possible to save the sent data because another record already exists with the same values',
 ];
