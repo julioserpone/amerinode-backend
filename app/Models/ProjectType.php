f791ce06a3a4c922f083ec5a1a56faa39f05d1ed
Julio Cesar Hernandez <juliohernandezs@gmail.com>
2022-07-08 17:30:12 -0500

    feat: project type migration

---
+++ app/Models/ProjectType.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class ProjectType extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'service_type_id',
+        'description',
+        'status',
+    ];
+}
---
+++ database/migrations/2022_07_08_153626_create_project_types_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('project_types', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('service_type_id')->constrained('service_types');
+            $table->string('description');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('project_types');
+    }
+};
