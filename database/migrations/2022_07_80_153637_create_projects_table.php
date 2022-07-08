9fe1863e12b52918e5388fa38c3243f3d6a69991
Julio Cesar Hernandez <juliohernandezs@gmail.com>
2022-07-08 17:31:11 -0500

    refactor: rename project migration

---
+++ database/migrations/2022_07_80_153637_create_projects_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('project_type_id')->constrained('project_types');
+            $table->foreignId('branch_id')->constrained('branches');
+            $table->string('name', 100);
+            $table->string('description');
+            $table->string('status')->default('active');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projects');
+    }
+};
