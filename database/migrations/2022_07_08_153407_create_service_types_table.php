a6b408a2e061cce424c645b574de2136289d813a
Julio Cesar Hernandez <juliohernandezs@gmail.com>
2022-07-08 17:29:59 -0500

    feat: service type migration

--- database/migrations/2022_07_08_153407_create_service_types_table.php
+++ database/migrations/2022_07_08_153407_create_service_types_table.php
@@ -13,8 +13,10 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('projects', function (Blueprint $table) {
+        Schema::create('service_types', function (Blueprint $table) {
             $table->id();
+            $table->string('description');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
@@ -26,6 +28,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('projects');
+        Schema::dropIfExists('service_types');
     }
 };
