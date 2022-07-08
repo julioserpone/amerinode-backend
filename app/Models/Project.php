a965d205cddbb56c1824d42ee0b58cea3d2b6e5e
Julio Cesar Hernandez <juliohernandezs@gmail.com>
2022-07-08 17:32:11 -0500

    refactor: soft delete in project model

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,8 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Project extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'project_type_id',
+        'branch_id',
+        'name',
+        'description',
+        'status',
+    ];
 }
