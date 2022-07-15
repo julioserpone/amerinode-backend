329a5f32722468a0325930e67ce1d465aee2b606
Julio Cesar Hernandez <juliohernandezs@gmail.com>
2022-07-15 17:27:12 -0500

    feat: update project

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -4,9 +4,11 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreProjectRequest;
 use App\Http\Requests\UpdateProjectRequest;
+use App\Models\Branch;
 use App\Models\Project;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Response;
 
 class ProjectController extends Controller
@@ -24,10 +26,10 @@ class ProjectController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \App\Http\Requests\StoreProjectRequest  $request
-     * @return Response
+     * @param  StoreProjectRequest  $request
+     * @return JsonResponse
      */
-    public function store(StoreProjectRequest $request)
+    public function store(StoreProjectRequest $request): JsonResponse
     {
         //
     }
@@ -59,20 +61,38 @@ class ProjectController extends Controller
      *
      * @param  UpdateProjectRequest  $request
      * @param  Project  $project
-     * @return Response
+     * @return JsonResponse
      */
-    public function update(UpdateProjectRequest $request, Project $project)
+    public function update(UpdateProjectRequest $request, Project $project): JsonResponse
     {
-        //
+        $branch = Branch::where('country_id', $request->country['id'])
+            ->where('company_id', $request->company['id'])
+            ->first();
+
+        $duplicate = Project::duplicate($request->project_type['id'], $branch->id, $request->project['name'], $request->project['description'], $project->id)->first();
+
+        if ($duplicate) {
+            return response()->json(['message' => __('notification.duplicated')], 409);
+        }
+
+        $project->update([
+            'name' => $request->project['name'],
+            'description' => $request->project['description'],
+            'project_type_id' => $request->project_type['id'],
+            'branch_id' => $branch->id,
+            'status' => $request->status['id'],
+        ]);
+
+        return response()->json(__('notification.updated', ['attribute' => 'project']));
     }
 
     /**
      * Remove the specified resource from storage.
      *
      * @param  Project  $project
-     * @return Response
+     * @return JsonResponse
      */
-    public function destroy(Project $project)
+    public function destroy(Project $project): JsonResponse
     {
         //
     }
