6ae1ff87a4ef60442d30b13e77d800fd3a499144
Julio Cesar Hernandez <juliohernandezs@gmail.com>
2022-07-25 18:11:34 -0500

    feat: add nomenclature data to unit seeder

--- database/seeders/UnitSeeder.php
+++ database/seeders/UnitSeeder.php
@@ -15,16 +15,17 @@ class UnitSeeder extends Seeder
     public function run()
     {
         $units = [
-            ['name' => 'minute', 'unit' => 'minute', 'description' => 'Minutes', 'type' => 'time', 'factor_conversion' => 60],
-            ['name' => 'hour', 'unit' => 'hour', 'description' => 'Hours', 'type' => 'time', 'factor_conversion' => 3600],
-            ['name' => 'working-hour', 'unit' => 'hour', 'description' => 'Workings Hours', 'type' => 'time', 'factor_conversion' => 3600],
-            ['name' => 'calendar-days', 'unit' => 'day', 'description' => 'Calendar days', 'type' => 'time,resolution', 'factor_conversion' => 86400, 'weekdays' => false],
-            ['name' => 'week', 'unit' => 'week', 'description' => 'Weeks', 'type' => 'time', 'factor_conversion' => 604800, 'weekdays' => false],
-            ['name' => '24*7', 'unit' => 'hour', 'description' => 'This refers to availability at all hours', 'type' => 'availability', 'factor_conversion' => null, 'weekdays' => false],
-            ['name' => '8*5', 'unit' => 'hour', 'description' => 'This refers to availability working hours only', 'type' => 'availability', 'factor_conversion' => null, 'weekdays' => false],
-            ['name' => 'next-business-day', 'unit' => 'day', 'description' => 'Next business day', 'type' => 'time,resolution', 'factor_conversion' => 86400, 'weekdays' => true],
-            ['name' => 'next-release', 'unit' => 'next release', 'description' => 'Next release', 'type' => 'resolution', 'factor_conversion' => null, 'weekdays' => false],
-            ['name' => 'not-available', 'unit' => 'N/A', 'description' => 'Not available', 'type' => 'time,resolution,availability', 'factor_conversion' => null, 'weekdays' => false],
+            ['name' => 'minute', 'unit' => 'minute', 'nomenclature' => 'min', 'description' => 'Minutes', 'type' => 'time', 'factor_conversion' => 60],
+            ['name' => 'hour', 'unit' => 'hour', 'nomenclature' => 'hrs', 'description' => 'Hours', 'type' => 'time', 'factor_conversion' => 3600],
+            ['name' => 'working-hour', 'unit' => 'hour', 'nomenclature' => 'WH', 'description' => 'Working Hours', 'type' => 'time', 'factor_conversion' => 3600],
+            ['name' => 'working-days', 'unit' => 'day', 'nomenclature' => 'WD', 'description' => 'Working Days', 'type' => 'time', 'factor_conversion' => 86400],
+            ['name' => 'calendar-days', 'unit' => 'day', 'nomenclature' => 'CD', 'description' => 'Calendar days', 'type' => 'time,resolution', 'factor_conversion' => 86400, 'weekdays' => false],
+            ['name' => 'week', 'unit' => 'week', 'nomenclature' => 'Week', 'description' => 'Weeks', 'type' => 'time', 'factor_conversion' => 604800, 'weekdays' => false],
+            ['name' => '24*7', 'unit' => 'hour', 'nomenclature' => null, 'description' => 'This refers to availability at all hours', 'type' => 'availability', 'factor_conversion' => null, 'weekdays' => false],
+            ['name' => '8*5', 'unit' => 'hour', 'nomenclature' => null, 'description' => 'This refers to availability working hours only', 'type' => 'availability', 'factor_conversion' => null, 'weekdays' => false],
+            ['name' => 'next-business-day', 'unit' => 'day', 'nomenclature' => 'NBD', 'description' => 'Next business day', 'type' => 'time,resolution', 'factor_conversion' => 86400, 'weekdays' => true],
+            ['name' => 'next-release', 'unit' => 'next release', 'nomenclature' => 'Next Rel', 'description' => 'Next release', 'type' => 'resolution', 'factor_conversion' => null, 'weekdays' => false],
+            ['name' => 'not-available', 'unit' => 'N/A', 'nomenclature' => 'N/A', 'description' => 'Not available', 'type' => 'time,resolution,availability', 'factor_conversion' => null, 'weekdays' => false],
         ];
 
         foreach ($units as $unit) {
